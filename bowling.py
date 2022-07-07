461ae8ea7b053e2817be02e01c2d59b401a22666
Thomas Errington <tom.errington58@gmail.com>
2022-07-07 15:10:55 -0400

    Added some comments

--- bowling.py
+++ bowling.py
@@ -2,27 +2,26 @@ class Bowling:
 
     def __init__(self):
         self.score = [[0 for x in range(2)] for y in range(10)]
-        self.score[9].append(0)
+        self.score[9].append(0) # adds a third roll to 10th frame
         self.frame = 0
         self.roll_count = 0
 
+    # Will fill in lists as a roll with amount of pins
     def roll(self, pins):
 
         if self.roll_count == 2 and self.frame != 9: # reset counts after second roll
             self.roll_count = 0
             self.frame += 1
 
-
         if self.roll_count == 0 and pins == 10 and self.frame != 9:  # checking for a strike here reset counts
             self.score[self.frame][self.roll_count] = pins
             self.roll_count = 2
         else:
-            self.score[self.frame][self.roll_count] = pins
+            self.score[self.frame][self.roll_count] = pins # normal adding
             self.roll_count += 1
 
-        # if self.frame == 9 and (self.is_strike(self.frame) or self.is_spare(self.frame)):
-        #     self.score[self.frame].append(0)
 
+    # Will generate the score after all rolls are completed
     def calculate_score(self):
         score = 0
         frame = 0
@@ -31,28 +30,32 @@ class Bowling:
             if frame == 8:
                 score += sum(self.score[9])
             else:  
-                if self.is_strike(frame):
+                if self.is_strike(frame): # need to look at this section. Strike rules don't make alot of sense
                     if self.score[frame][0] == 10 and self.score[frame + 1][0] == 10:
                         score += 10 + self.score[frame + 1][0] + self.score[frame + 2][0]
                     else:
                         score += 10 + self.score[frame + 1][0] + self.score[frame + 1][1]
-                elif self.is_spare(frame):
+                elif self.is_spare(frame):  # spare calculation
                     score += 10 + self.score[frame + 1][0]
-                else:
+                else: # normal calculation
                     score += self.score[frame][0] + self.score[frame][1]
                 frame += 1
 
         return score
 
+    # Helper to test if roll is a strike
     def is_strike(self,frame):
         return self.score[frame][0] == 10
     
+
+    # Helper to test if roll is a spare
     def is_spare(self, frame):
         return self.score[frame][0] + self.score[frame][1] == 10
 
 
 if __name__ == "__main__":
 
+    # Test game
     game = Bowling()
 
     for x in range(21):
--- test.py
+++ test.py
@@ -3,36 +3,46 @@ from bowling import Bowling
 
 class Tests(unittest.TestCase):
 
+    # Instantiate Game for all tests
     def setUp(self):
         self.game = Bowling()
 
+    # Helper function to do 20 rolls
     def roller(self, pins):
         for x in range(20):
             self.game.roll(pins)
     
+    # Helper to roll all strikes
     def strike_roller(self, pins):
         for x in range(12):
             self.game.roll(pins)
+
+    # Helper to roll all spares
     def spare_roller(self,pins):
         for x in range(21):
             self.game.roll(5)
 
+    # No pins knocked down
     def test_all_zeros(self):
         self.roller(0)
         self.assertEqual(self.game.calculate_score(), 0)
 
+    # One pin per roll knocked down
     def test_all_ones(self):
         self.roller(1)
         self.assertEqual(self.game.calculate_score(), 20)
 
+    # Game of all spares
     def test_all_spares(self):
         self.spare_roller(5)
         self.assertEqual(self.game.calculate_score(), 150)
 
+    # Game of all strikes
     def test_all_strikes(self):
         self.strike_roller(10)
         self.assertEqual(self.game.calculate_score(), 300)
 
+    # Just a single spare
     def test_single_spare(self):
         self.game.roll(5)
         self.game.roll(5)
@@ -40,6 +50,7 @@ class Tests(unittest.TestCase):
             self.game.roll(0)
         self.assertEqual(self.game.calculate_score(), 10)
 
+    # Just a single strike
     def test_single_strike(self):
         self.game.roll(10)
         self.game.roll(4)
@@ -48,6 +59,7 @@ class Tests(unittest.TestCase):
             self.game.roll(0)
         self.assertEqual(self.game.calculate_score(), 20)
 
+    # Random game I hand calculate the total
     def test_random_game(self):
         self.game.roll(2)
         self.game.roll(3)
@@ -69,8 +81,6 @@ class Tests(unittest.TestCase):
         self.game.roll(10)
         self.game.roll(10)
         self.assertEqual(self.game.calculate_score(), 137)
-
-
 
 
 if __name__ == '__main__':
