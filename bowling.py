79a35730460f33a59a76180984392aa5c5f39f22
Thomas Errington <tom.errington58@gmail.com>
2022-07-07 15:00:00 -0400

    working game, no input validation

--- bowling.py
+++ bowling.py
@@ -2,33 +2,44 @@ class Bowling:
 
     def __init__(self):
         self.score = [[0 for x in range(2)] for y in range(10)]
+        self.score[9].append(0)
         self.frame = 0
         self.roll_count = 0
 
     def roll(self, pins):
 
-        if self.roll_count == 2: # reset counts after second roll
+        if self.roll_count == 2 and self.frame != 9: # reset counts after second roll
             self.roll_count = 0
             self.frame += 1
 
-        if self.roll_count == 0 and pins == 10:  # checking for a strike here reset counts
+
+        if self.roll_count == 0 and pins == 10 and self.frame != 9:  # checking for a strike here reset counts
             self.score[self.frame][self.roll_count] = pins
             self.roll_count = 2
         else:
             self.score[self.frame][self.roll_count] = pins
             self.roll_count += 1
 
+        # if self.frame == 9 and (self.is_strike(self.frame) or self.is_spare(self.frame)):
+        #     self.score[self.frame].append(0)
 
     def calculate_score(self):
         score = 0
         frame = 0
         for x in range(10):
-            if self.is_strike(frame):
-                score += 10 + self.score[frame + 1][0] + self.score[frame + 1][1]
-            elif self.is_spare(frame):
-                score += 10 + self.score[frame + 1][0]
-            else:
-                score += self.score[frame][0] + self.score[frame][1]
+
+            if frame == 8:
+                score += sum(self.score[9])
+            else:  
+                if self.is_strike(frame):
+                    if self.score[frame][0] == 10 and self.score[frame + 1][0] == 10:
+                        score += 10 + self.score[frame + 1][0] + self.score[frame + 2][0]
+                    else:
+                        score += 10 + self.score[frame + 1][0] + self.score[frame + 1][1]
+                elif self.is_spare(frame):
+                    score += 10 + self.score[frame + 1][0]
+                else:
+                    score += self.score[frame][0] + self.score[frame][1]
                 frame += 1
 
         return score
@@ -44,7 +55,7 @@ if __name__ == "__main__":
 
     game = Bowling()
 
-    for x in range(20):
+    for x in range(21):
         game.roll(5)
 
     print(game.score)
--- test.py
+++ test.py
@@ -11,8 +11,11 @@ class Tests(unittest.TestCase):
             self.game.roll(pins)
     
     def strike_roller(self, pins):
-        for x in range(10):
+        for x in range(12):
             self.game.roll(pins)
+    def spare_roller(self,pins):
+        for x in range(21):
+            self.game.roll(5)
 
     def test_all_zeros(self):
         self.roller(0)
@@ -22,24 +25,50 @@ class Tests(unittest.TestCase):
         self.roller(1)
         self.assertEqual(self.game.calculate_score(), 20)
 
-    def test_all_spares_no_bonus(self):
-        self.roller(5)
-        self.assertEqual(self.game.calculate_score(), 100)
-
-    def test_all_strikes_no_bonus(self):
-        self.strike_roller(10)
-        self.assertEqual(self.game.calculate_score(), 100)
-
     def test_all_spares(self):
-        self.roller(5)
+        self.spare_roller(5)
         self.assertEqual(self.game.calculate_score(), 150)
 
     def test_all_strikes(self):
         self.strike_roller(10)
         self.assertEqual(self.game.calculate_score(), 300)
 
-    def test_ace_tenth(self):
-        return 1
+    def test_single_spare(self):
+        self.game.roll(5)
+        self.game.roll(5)
+        for x in range(1,10):
+            self.game.roll(0)
+        self.assertEqual(self.game.calculate_score(), 10)
+
+    def test_single_strike(self):
+        self.game.roll(10)
+        self.game.roll(4)
+        self.game.roll(1)
+        for x in range(2,10):
+            self.game.roll(0)
+        self.assertEqual(self.game.calculate_score(), 20)
+
+    def test_random_game(self):
+        self.game.roll(2)
+        self.game.roll(3)
+        self.game.roll(10)
+        self.game.roll(8)
+        self.game.roll(1)
+        self.game.roll(4)
+        self.game.roll(6)
+        self.game.roll(4)
+        self.game.roll(4)
+        self.game.roll(4)
+        self.game.roll(5)
+        self.game.roll(9)
+        self.game.roll(0)
+        self.game.roll(2)
+        self.game.roll(2)
+        self.game.roll(10)
+        self.game.roll(10)
+        self.game.roll(10)
+        self.game.roll(10)
+        self.assertEqual(self.game.calculate_score(), 137)
 
 
 
