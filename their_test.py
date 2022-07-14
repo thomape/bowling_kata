252175dc92d0c9e88a84f3f69a2c6289bb7669e3
Thomas Errington <tom.errington58@gmail.com>
2022-07-14 14:11:04 -0400

    changes module name

--- their_test.py
+++ their_test.py
@@ -1,22 +1,22 @@
 import unittest
-import game
+import their_main
 
 class Tests(unittest.TestCase):
 
     def test_ones(self):
-        self.assertEqual(game.score([1,1,1,1]),4)
+        self.assertEqual(their_main.score([1,1,1,1]),4)
 
     def test_strike(self):
-        self.assertEqual(game.score([10,1,1,1,1]),16)
+        self.assertEqual(their_main.score([10,1,1,1,1]),16)
 
     def test_perfect(self):
-        self.assertEqual(game.score([10,10,10,10,10,10,10,10,10,10,10,10]),300)
+        self.assertEqual(their_main.score([10,10,10,10,10,10,10,10,10,10,10,10]),300)
 
     def test_spare(self):
-        self.assertEqual(game.score([5,5,1,1]),13)
+        self.assertEqual(their_main.score([5,5,1,1]),13)
 
     def test_custom(self):
-        self.assertEqual(game.score([1,1,2,2,3,3,4,4,5,5,6,0,7,0,8,0,9,0,10,1,1]),78)
+        self.assertEqual(their_main.score([1,1,2,2,3,3,4,4,5,5,6,0,7,0,8,0,9,0,10,1,1]),78)
 
 
 if __name__=='__main__':
